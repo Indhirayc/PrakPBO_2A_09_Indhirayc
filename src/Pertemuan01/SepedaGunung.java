2fcca009e8978f335b15d67fac13f55381239afe
Indhirayc <indhirayuantikac1176@gmail.com>
2025-08-28 19:51:17 +0700

    Percobaan2

---
+++ src/Pertemuan01/SepedaGunung.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pertemuan01;
+
+/**
+ *
+ * @author WINDOWS 11
+ */
+public class SepedaGunung extends Sepeda {
+    private String tipeSuspensi;
+
+    public void setTipeSuspensi(String newValue)
+    {
+        tipeSuspensi = newValue;
+    }
+
+    public void cetakStatus()
+    {
+        super.cetakStatus();
+        System.out.println("Tipe suspensi: " + tipeSuspensi);
+    }
+}

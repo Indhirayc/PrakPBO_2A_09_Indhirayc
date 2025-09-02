ed562bc4d7e5ff6f1fe39b54c603ccd170015e76
Indhirayc <indhirayuantikac1176@gmail.com>
2025-09-02 17:21:37 +0700

    Percobaan1_PertanyaanNo12

--- src/Pertemuan02/TestMahasiswa.java
+++ src/Pertemuan02/TestMahasiswa.java
@@ -11,10 +11,25 @@ package Pertemuan02;
 public class TestMahasiswa {
     public static void main(String[] args) {
         Mahasiswa mhs1 = new Mahasiswa();
+        Mahasiswa mhs2 = new Mahasiswa();
+        Mahasiswa mhs3 = new Mahasiswa();
+
         mhs1.nim = 101;
         mhs1.nama = "Lestari";
         mhs1.alamat = "Jl. Vinolia No 1A";
         mhs1.kelas = "1A";
         mhs1.tampilBiodata();
+        
+        mhs2.nim = 102;
+        mhs2.nama = "Ozora";
+        mhs2.alamat = "Jl. Dewi Sartika No 7";
+        mhs2.kelas = "1B";
+        mhs2.tampilBiodata();
+        
+        mhs3.nim = 103;
+        mhs3.nama = "Pratama";
+        mhs3.alamat = "Jl. Sultan Agung No 3A";
+        mhs3.kelas = "1A";
+        mhs3.tampilBiodata();
     }
 }

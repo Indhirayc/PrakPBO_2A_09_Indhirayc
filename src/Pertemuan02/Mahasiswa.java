692bfafb97babd3053c5532498aaea6f113ecb5e
Indhirayc <indhirayuantikac1176@gmail.com>
2025-09-02 17:18:01 +0700

    Percobaan1

---
+++ src/Pertemuan02/Mahasiswa.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pertemuan02;
+
+/**
+ *
+ * @author WINDOWS 11
+ */
+public class Mahasiswa {
+    public int nim;
+    public String nama, alamat, kelas;
+    
+    public void tampilBiodata() {
+        System.out.println("NIM : " + nim);
+        System.out.println("Nama : " + nama);
+        System.out.println("Alamat : " + alamat);
+        System.out.println("Kelas : " + kelas);
+    }
+}
